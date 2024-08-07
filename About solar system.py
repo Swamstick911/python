be86f642b6d889ca57f6773d3f1eb532089480be
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2024-08-07 18:55:08 +0530

    Create About solar system.py

---
+++ About solar system.py
@@ -0,0 +1,17 @@
+# About Our Solar System
+
+import time
+
+print("Hello there Astronomer, nice to see you")
+time.sleep(1)
+print("Today we will learn about our solar system")
+time.sleep(1)
+print("Our solar system consisted of 9 planets until 2006 when International Astronomical Union (IAU) declared it was a dwarf planet")
+time.sleep(2)
+print("You might think what is a dwarf planet?")
+time.sleep(2)
+print("Think about it yourself for sometime...")
+time.sleep(7)
+print("A dwarf planet is a small planetary-mass object that is in direct orbit around the Sun")
+time.sleep(2)
+print("")
