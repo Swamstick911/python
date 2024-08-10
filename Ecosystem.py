10c45cb76bff9125c441c567f7823d7264b43fd4
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2024-08-10 16:01:08 +0530

    Create Ecosystem.py

---
+++ Ecosystem.py
@@ -0,0 +1,16 @@
+import time
+
+name = input("Enter your name: ")
+print(f"Hello {name}, Welcome to Ecosystem!\n")
+print("In this program you will learn about Ecosystem!")
+time.sleep(1)
+print("Most of you might think learning about Ecosystem is boring, but as we will get deeper into it, you'll love it!")
+time.sleep(1)
+print("Let's start with the basics")
+time.sleep(2)
+print("You might think of what is an Ecosystem")
+time.sleep(1)
+print("Ecosystem is a system of interactions between living things")
+time.sleep(1)
+print("Ecosystem is made up of both living (biotic) and non-living things (abiotic)")
+time.sleep(1)
